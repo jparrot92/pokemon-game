af48af3243fa3fb9c6d0c23f9c2de863e21caa91
Jaume Parrot <jparrot@nalandaglobal.com>
2022-07-15 08:40:12 +0200

    Pruebas con emisiones

--- tests/unit/components/PokemonOptions.spec.js
+++ tests/unit/components/PokemonOptions.spec.js
@@ -23,4 +23,33 @@ describe('PokemonOptions Component', () => {
 
     })
 
+    test('debe de mostrar las 4 opciones correctamente', () => {
+
+        const liTags = wrapper.findAll('li')
+        expect(liTags.length).toBe(4)
+
+        expect(liTags[0].text()).toBe('bulbasaur')
+        expect(liTags[1].text()).toBe('ivysaur')
+        expect(liTags[2].text()).toBe('venusaur')
+        expect(liTags[3].text()).toBe('charmander')
+
+
+    })
+
+    test('debe de emitir "selection" con sus respectivos parámetros al hacer click', () => {
+
+        const [li1, li2, li3, li4 ] = wrapper.findAll('li')
+
+        li1.trigger('click')
+        li2.trigger('click')
+        li3.trigger('click')
+        li4.trigger('click')
+        //console.log(wrapper.emitted('selectionPokemon'))
+        expect(wrapper.emitted('selectionPokemon').length).toBe(4)
+        expect(wrapper.emitted('selectionPokemon')[0]).toEqual([1])
+        expect(wrapper.emitted('selectionPokemon')[1]).toEqual([2])
+        expect(wrapper.emitted('selectionPokemon')[2]).toEqual([3])
+        expect(wrapper.emitted('selectionPokemon')[3]).toEqual([4])
+    })
+
 })
