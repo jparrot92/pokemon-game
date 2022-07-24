ae257ee7571376a19a4dd7be4e96bd1f83b983b1
Jaume Parrot <jparrot@nalandaglobal.com>
2022-07-24 18:58:09 +0200

    Pruebas las propiedades reactivas del componente

--- tests/unit/pages/PokemonPage.spec.js
+++ tests/unit/pages/PokemonPage.spec.js
@@ -40,7 +40,7 @@ describe('PokemonPage Component', () => {
 
     })
 
-    test.only('debe de mostrar los componentes de PokemonPicture y PokemonOptions', () => {
+    test('debe de mostrar los componentes de PokemonPicture y PokemonOptions', () => {
 
         const wrapper = shallowMount(PokemonPage, {
             data(){
@@ -67,6 +67,32 @@ describe('PokemonPage Component', () => {
 
         //PokemonOptions attibute pokemons toBe true
         expect(pokemonOptions.attributes('pokemons')).toBeTruthy()
+        
+    })
+
+    test('prubas con checkAnswer', async() => {
+
+        const wrapper = shallowMount(PokemonPage, {
+            data(){
+                return {
+                    pokemonArr: pokemons,
+                    pokemon: pokemons[0],
+                    showPokemon: false,
+                    showAnswer: false,
+                    message: ''
+                }
+            }
+        })
+
+        await wrapper.vm.checkAnswer(1)
+
+        expect( wrapper.find('h2').exists() ).toBeTruthy()
+        expect( wrapper.vm.showPokemon ).toBe(true)
+        expect( wrapper.find('h2').text() ).toBe(`Correcto, ${ pokemons[0].name }`)
+
+        await wrapper.vm.checkAnswer(10)
+
+        expect( wrapper.vm.message ).toBe(`Oops, era ${ pokemons[0].name }`)
         
     })
 })
